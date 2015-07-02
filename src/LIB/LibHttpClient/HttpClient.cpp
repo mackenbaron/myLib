264aceacec8ba45b25834b054a789c03808f7676
DayBreakZhang <a365316465@126.com>
2015-07-02 14:26:57 +0800

    add
    add

---
+++ src/LIB/LibHttpClient/HttpClient.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "HttpClient.h"
+
+
+HttpClient::HttpClient(void)
+{
+}
+
+
+HttpClient::~HttpClient(void)
+{
+}
---
+++ src/LIB/LibJson/ParseJson.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "ParseJson.h"
+
+
+ParseJson::ParseJson(void)
+{
+}
+
+
+ParseJson::~ParseJson(void)
+{
+}
