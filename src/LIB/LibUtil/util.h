40021d425efc9b25b91f1e3c8fbcc4d3dacb3fef
DayBreakZhang <a365316465@126.com>
2015-07-29 10:07:56 +0800

    add
    add

--- src/LIB/LibUtil/util.h
+++ src/LIB/LibUtil/util.h
@@ -2,24 +2,30 @@
 #define UTIL_H
 
 class util
-{
-public:
+	{
+	public:
+#ifdef WIN32
+		static void readINIFileString(std::string path,std::string root,std::string userkey,std::string &uservalue,std::string def);
+		static void readINIFileInt(std::string path,std::string root,std::string userkey,int &userValue,int def);
+		static void writeINIFileString(std::string path,std::string root,std::string userkey,std::string value);
+#endif
+		static void UTF_8ToUnicode(wchar_t* pOut,char *pText);  // 把UTF-8转换成Unicode
+		static void UnicodeToUTF_8(char* pOut,wchar_t* pText);  //Unicode 转换成UTF-8
+		static void UnicodeToGB2312(char* pOut,wchar_t uData);  // 把Unicode 转换成 GB2312
+		static void Gb2312ToUnicode(wchar_t* pOut,char *gbBuffer);// GB2312 转换成　Unicode
+		static void GB2312ToUTF_8(std::string& pOut,char *pText, int pLen);//GB2312 转为 UTF-8
+		static void UTF_8ToGB2312(std::string &pOut, char *pText, int pLen);//UTF-8 转为 GB2312
+		static WCHAR* AnsiToUnicode( const char* szStr );//char to wchar
+		static char* UnicodeToAnsi( const WCHAR* szStr );//wchar to char
+		static char* UnicodeToUTF8( const WCHAR* szStr );//wchar to utf8
+		static wchar_t* Utf8ToUnicode(const char* utf);//utf8 to wchar
+		static char* intTOStirng(const int n);
+		static char* DoubleTOString(const double n);
+		static int StringToInt(const char* src);
+		static double StringTODouble(const char* src);
 
-	void UTF_8ToUnicode(wchar_t* pOut,char *pText);  // 把UTF-8转换成Unicode
-	void UnicodeToUTF_8(char* pOut,wchar_t* pText);  //Unicode 转换成UTF-8
-	void UnicodeToGB2312(char* pOut,wchar_t uData);  // 把Unicode 转换成 GB2312
-	void Gb2312ToUnicode(wchar_t* pOut,char *gbBuffer);// GB2312 转换成　Unicode
-	void GB2312ToUTF_8(std::string& pOut,char *pText, int pLen);//GB2312 转为 UTF-8
-	void UTF_8ToGB2312(std::string &pOut, char *pText, int pLen);//UTF-8 转为 GB2312
-	WCHAR* AnsiToUnicode( const char* szStr );//char to wchar
-	char* UnicodeToAnsi( const WCHAR* szStr );//wchar to char
-	char* UnicodeToUTF8( const WCHAR* szStr );//wchar to utf8
-	wchar_t* Utf8ToUnicode(const char* utf);//utf8 to wchar
-	char* intTOStirng(const int n);
-	char* DoubleTOString(const double n);
-	int StringToInt(const char* src);
-	double StringTODouble(const char* src);
-	
 };
+
+
 
 #endif // UTIL_H
