95f816916a36baf4e33711465b3a1725c803c6fe
DayBreakZhang <a365316465@126.com>
2015-09-17 18:17:57 +0800

    add
    add

--- src/TestLib/testhttpclient/testhttpclient.cpp
+++ src/TestLib/testhttpclient/testhttpclient.cpp
@@ -6,14 +6,14 @@
 
 int main(int argc, char* argv[])
 {
-	std::string url="http://220.181.112.244:80/index.html";///"http://192.168.0.160:8089/Engine/EngInfo";
-	//std::string pro="";
-	//pro="TaskID=123456&DevID=123456&State=0&Message=4165412";
+	std::string url="http://192.168.0.160:8089/Engine/EngInfo";
+	std::string pro="";
+	pro="TaskID=123456&DevID=123456&State=0&Message=4165412";
 
 	HttpClient cl;
 	std::string _tempa="";
 	std::string _tempb ="";
-	cl.clientGet(url,_tempa,_tempb);
+	cl.clientPost(url,pro,_tempa,_tempb);
 
 	return 0;
 }
