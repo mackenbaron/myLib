8356ffadd890f906e26a96db806182acb317723f
DayBreakZhang <a365316465@126.com>
2015-09-18 13:22:41 +0800

    add
    添加新的httpclient get post 的方法

--- src/LIB/LibHttpClient/HttpClient.cpp
+++ src/LIB/LibHttpClient/HttpClient.cpp
@@ -46,3 +46,82 @@ int HttpClient::ClientDownload(IN std::string sourSrc,IN std::string destSrc,IN 
 	downloadClient.SaveResponseToFile(u.AnsiToUnicode(destSrc.c_str()));
 	return 0;
 }
+
+int HttpClient::clientNewGet(IN std::string url, 
+	OUT std::string &ResponseHeader,OUT std::string &ResponseContent, 
+	IN int resolveTimeout/*=0*/,IN int connectTimeout /*= 60000*/,IN int sendTimeout /*= 30000*/,IN int receiveTimeout /*= 30000*/, 
+	IN std::string UserAgent/*=""*/,IN std::string Proxy/*=""*/,IN std::string Cookies/*=""*/)
+{
+	util u;
+	// Set URL.
+	WinHttpClient client(u.AnsiToUnicode(url.c_str()));
+	client.SetTimeouts(resolveTimeout,connectTimeout,sendTimeout,receiveTimeout);
+
+	if(!UserAgent.empty())
+	{
+		client.SetUserAgent(u.AnsiToUnicode(UserAgent.c_str()));
+	}
+	if(!Proxy.empty())
+	{
+		client.SetProxy(u.AnsiToUnicode(Proxy.c_str()));
+	}
+	if(!Cookies.empty())
+	{
+		client.SetAdditionalRequestCookies(u.AnsiToUnicode(Cookies.c_str()));
+	}
+	// Send http request, a GET request by default.
+	client.SendHttpRequest();
+
+	// The response header.
+	wstring httpResponseHeader = client.GetResponseHeader();
+	ResponseHeader = u.UnicodeToAnsi(httpResponseHeader.c_str());
+	// The response content.
+	wstring httpResponseContent = client.GetResponseContent();
+	ResponseContent = u.UnicodeToAnsi(httpResponseContent.c_str());
+	return _wtoi(client.GetResponseStatusCode().c_str());
+}
+
+int HttpClient::clientNewPost(IN std::string url,IN std::string contentData, 
+	OUT std::string &ResponseHeader,OUT std::string &ResponseContent, 
+	IN int resolveTimeout/*=0*/,IN int connectTimeout /*= 60000*/,IN int sendTimeout /*= 30000*/,IN int receiveTimeout /*= 30000*/, 
+	IN std::string UserAgent/*=""*/,IN std::string Proxy/*=""*/,IN std::string Cookies/*=""*/)
+{
+	util u;
+	WinHttpClient client(u.AnsiToUnicode(url.c_str()));
+	client.SetTimeouts(resolveTimeout,connectTimeout,sendTimeout,receiveTimeout);
+	// Set post data.
+	string data = contentData;
+	if(!contentData.empty())
+	{
+		client.SetAdditionalDataToSend((BYTE *)data.c_str(), data.size());
+	}
+	if(!UserAgent.empty())
+	{
+		client.SetUserAgent(u.AnsiToUnicode(UserAgent.c_str()));
+	}
+	if(!Proxy.empty())
+	{
+		client.SetProxy(u.AnsiToUnicode(Proxy.c_str()));
+	}
+	if(!Cookies.empty())
+	{
+		client.SetAdditionalRequestCookies(u.AnsiToUnicode(Cookies.c_str()));
+	}
+	// Set request headers.
+	wchar_t szSize[50] = L"";
+	swprintf_s(szSize, L"%d", data.size());
+	wstring headers = L"Content-Length: ";
+	headers += szSize;
+	headers += L"\r\nContent-Type: application/x-www-form-urlencoded\r\n";
+	client.SetAdditionalRequestHeaders(headers);
+
+	// Send http post request.
+	client.SendHttpRequest(L"POST");
+
+	wstring httpResponseHeader = client.GetResponseHeader();
+	wstring httpResponseContent = client.GetResponseContent();
+
+	ResponseHeader = u.UnicodeToAnsi(httpResponseHeader.c_str());
+	ResponseContent = u.UnicodeToAnsi(httpResponseContent.c_str());
+	return _wtoi(client.GetResponseStatusCode().c_str());
+}
