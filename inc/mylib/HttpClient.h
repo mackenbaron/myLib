2eb2900c4e1bb7c8188c3c83133877b5f8363a85
DayBreakZhang <a365316465@126.com>
2015-09-18 13:36:36 +0800

    add
    add

--- inc/mylib/HttpClient.h
+++ inc/mylib/HttpClient.h
@@ -14,6 +14,16 @@ public:
 		OUT std::string &ResponseHeader,OUT std::string &ResponseContent,
 		IN std::string UserAgent="",IN std::string Prox="",IN std::string Cookies="");
 
+	int clientNewGet(IN std::string url,
+		OUT std::string &ResponseHeader,OUT std::string &ResponseContent,
+		IN int resolveTimeout=0,IN int connectTimeout = 60000,IN int sendTimeout = 30000,IN int receiveTimeout = 30000,
+		IN std::string UserAgent="",IN std::string Prox="",IN std::string Cookies="");
+
+	int clientNewPost(IN std::string url,IN std::string contentData,
+		OUT std::string &ResponseHeader,OUT std::string &ResponseContent,
+		IN int resolveTimeout=0,IN int connectTimeout = 60000,IN int sendTimeout = 30000,IN int receiveTimeout = 30000,
+		IN std::string UserAgent="",IN std::string Prox="",IN std::string Cookies="");
+
 
 	int ClientDownload(IN std::string sourSrc,IN std::string destSrc,IN std::string Cookies);
 };
